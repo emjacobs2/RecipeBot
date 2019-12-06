e98a3d2a950445d644b496a4bb087e21effb75c3
emjacobs2 <54752865+emjacobs2@users.noreply.github.com>
2019-12-06 16:55:09 -0600

    Add project files.

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,69 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.6.2
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Linq;
+using Microsoft.Bot.Builder.AI.QnA;
+
+namespace RecipeBot_2.Bots
+{
+
+    public class EchoBot : ActivityHandler
+    {
+        public QnAMaker EchoBotQnA { get; private set; }
+        public EchoBot(QnAMakerEndpoint endpoint)
+        {
+            // connects to QnA Maker endpoint for each turn
+            EchoBotQnA = new QnAMaker(endpoint);
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync(MessageFactory.Text($"Echo: {turnContext.Activity.Text}"), cancellationToken);
+
+            await AccessQnAMaker(turnContext, cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"Hello and welcome!"), cancellationToken);
+                }
+            }
+        }
+
+        private async Task AccessQnAMaker(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var results = await EchoBotQnA.GetAnswersAsync(turnContext);
+            if (results.Any())
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("QnA Maker Returned: " + results.First().Answer), cancellationToken);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("Sorry, could not find an answer in the Q and A system."), cancellationToken);
+            }
+        }
+
+        private IActivity CreateActivityWithTextAndSpeak(string message)
+        {
+            var activity = MessageFactory.Text(message);
+            string speak = @"<speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' xml:lang='en-US'>
+              <voice name='Microsoft Server Speech Text to Speech Voice (en-US, JessaRUS)'>" +
+              $"{message}" + "</voice></speak>";
+            activity.Speak = speak;
+            return activity;
+        }
+
+
+    }
+}
